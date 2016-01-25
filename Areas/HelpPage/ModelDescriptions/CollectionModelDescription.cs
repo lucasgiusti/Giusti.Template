223a833ea5ae854110ddc6c344d6053c52cca751
Lucas Giusti <giusti.lucas@gmail.com>
2016-01-25 18:21:17 -0300

    refazendo projeto web

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Giusti.Template.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
