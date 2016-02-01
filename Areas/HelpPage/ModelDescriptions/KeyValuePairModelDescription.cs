66f9f1a016d4339c128fdd5cf773ae0ba015ea98
Lucas Giusti <giusti.lucas@gmail.com>
2016-02-01 19:52:48 -0200

    Delete KeyValuePairModelDescription.cs

--- Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Giusti.Template.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
