6b2150b4d9286111221fb704916d616ab1428e2f
Lucas Giusti <giusti.lucas@gmail.com>
2016-02-01 19:51:42 -0200

    Delete CollectionModelDescription.cs

--- Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Giusti.Template.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
